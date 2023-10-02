64cada4e78d5e337925efa6b4e582999afbc343b
Garcia <Gi.T.Garcia@baypath.net>
2023-10-02 14:23:43 -0400

    Fixed shit fuck this amen

--- Assets/DamageSystem.cs
+++ Assets/DamageSystem.cs
@@ -5,35 +5,34 @@ using UnityEngine;
 
 public class DamageSystem : MonoBehaviourPunCallbacks
 {
-
+    Vector3 worldPosition;
     void Update()
     {
+        worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        int layerMask = 1 << 3;
+        layerMask = ~layerMask;
 
         if (Input.GetButtonDown("Fire1"))
         {
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), new Vector2(worldPosition.x, worldPosition.y), Mathf.Infinity, LayerMask.GetMask("Shootable"), 0);
+            if (hit.collider != null)
+            {
+                //Hit something, print the tag of the object
+                Debug.Log("Hitting: " + hit.collider.tag);
+                hit.collider.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+            }
             //photonView.RPC("Shoot", RpcTarget.All);
         }
     }
 
     [PunRPC]
-    private void Shoot(Ray ray)
+    private void Shoot()
     {
+        //List<RaycastHit2D> results = new List<RaycastHit2D>();
 
-        int layerMask = 1 << 3;
 
-        layerMask = ~layerMask;
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
-        {
-            Debug.DrawLine(ray.origin, hit.point);
-            Debug.Log("Did Hit");
-        }
-        else
-        {
-            Debug.Log("Did not Hit");
-        }
+       
     }
 
 }
